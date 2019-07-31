9cd0b53aca5e648cf836a9e8072885d0802193cf
Anna Verner <annverner1997@gmail.com>
2019-07-31 11:52:35 +0300

    first commit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,9 +9,9 @@ export class ProjectsComponent {
   over = false;
   itemindex;
   projects = [
-    {href: 'https://ucsr.kiev.ua/',  src: 'url("../../assets/img/ucsr.png")', technologies: ['HTML', 'CSS', 'JavaScript', 'jQuery']},
-    {href: 'https://annaverner.github.io/weather/',  src: 'url("../../assets/img/weather.png")', technologies: ['HTML', 'CSS', 'Sass', 'Angular']},
-    {href: 'https://idss.org.ua/holodomor',  src: 'url("../../assets/img/holodomor.jpg")', technologies: ['HTML', 'CSS', 'Sass', 'JavaScript', 'jQuery']}
+    {href: 'https://ucsr.kiev.ua/',  src: 'url("./assets/img/ucsr.png")', technologies: ['HTML', 'CSS', 'JavaScript', 'jQuery']},
+    {href: 'https://annaverner.github.io/weather/',  src: 'url("./assets/img/weather.png")', technologies: ['HTML', 'CSS', 'Sass', 'Angular']},
+    {href: 'https://idss.org.ua/holodomor',  src: 'url("./assets/img/holodomor.jpg")', technologies: ['HTML', 'CSS', 'Sass', 'JavaScript', 'jQuery']}
   ];
   showTechnologies(i) {
     this.over = !this.over;
